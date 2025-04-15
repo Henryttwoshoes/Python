157c51e4824d19f9e77073856599bd66e9b51239
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-04-15 12:34:54 -0300

    fiz a APS de matemática aplicada a TI

---
+++ Matlib/APS Luciana 2025.1/APS.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import statistics
+import pandas as pd
+
+dados=pd.read_excel(r'C:\Users\Usuário\Desktop\Gravações entre outros bens especiais\Programação\Python\Python\Matlib\APS Luciana 2025.1\tabela para exercício no excel dados de alunos.xlsx')
+
+x = (1,3,5,7,9,11,13,15)
+y = (2,4,6,8,10,13,15,18)
+
+#plt.title('Linhas legais')
+#plt.xlabel('Tempo')
+#plt.ylabel('Quantidade')
+
+print(dados)
+
+#plt.barh(x,y)
+#plt.show()
