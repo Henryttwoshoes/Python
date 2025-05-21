d232290ac75ae3e50060884d61bdd885df0497e2
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-05-21 13:51:28 -0300

    fiz a aula 6 do curso

---
+++ Curso em Video Python/ambiente de teste/aula06a.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor '))
+print(type(n1))
+s = n1 + n1
+print(f'n1 + ele mesmo é {s}')
---
+++ Curso em Video Python/exercícios/ex004.py
@@ -0,0 +1,4 @@
+algo = input("Digite algo ")
+print(type(algo))
+print(f"É numérico? = {algo.isnumeric}")
+print(f"É alfabético = {algo.isalpha}")
