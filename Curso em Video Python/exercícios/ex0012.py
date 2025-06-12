70810645f617fd42e58493c5b5c259cf64993cee
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-06-12 13:13:42 -0300

    resolvi os exercícios 12 e 13

---
+++ Curso em Video Python/exercícios/ex0012.py
@@ -0,0 +1,3 @@
+valor = float(input("Digite o preço: "))
+valorfinal = valor - (valor*0.05)
+print(f'O valor final do produto com 5% de desconto é R${valorfinal:.2f}.')
---
+++ Curso em Video Python/exercícios/ex0013.py
@@ -0,0 +1,3 @@
+atual = float(input('Digite o seu salário atual:'))
+novo = atual + (atual*0.15)
+print(f'Seu novo salário, agora com 15% de aumento é de R${novo:.2f}.')
